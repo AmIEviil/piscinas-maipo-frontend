6fa963229b3af372174c7d93308844fba12de7ce
GonzaloCaroK <gonzalo.caro@kabeli.cl>
2025-06-22 13:40:06 -0400

    Refactor code structure for improved readability and maintainability

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate, useLocation } from "react-router";
 import { PAGE_ROUTES } from "../../constant/routes";
 import style from "./TopBar.module.css";
-
+import PiscinasElMaipoIcon from "../ui/Icons/piscinasDelMaipoIcon";
 export const TopBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -11,13 +11,16 @@ export const TopBar = () => {
   return (
     <div className={style.topBarContainer}>
       <div className={style.headerModuleContainer}>
+        <PiscinasElMaipoIcon size={90} color="white" className={style.iconHeader}/>
         <span>Piscinas El Maipo</span>
       </div>
       <span>{actualRoute.charAt(0).toUpperCase() + actualRoute.slice(1)}</span>
       <div className={style.tabsContainer}>
         <button onClick={() => navigate("/")}>Home</button>
         <button onClick={() => navigate(PAGE_ROUTES.Clientes)}>Clientes</button>
-        <button onClick={() => navigate(PAGE_ROUTES.Inventario)}>Inventario</button>
+        <button onClick={() => navigate(PAGE_ROUTES.Inventario)}>
+          Inventario
+        </button>
       </div>
     </div>
   );
